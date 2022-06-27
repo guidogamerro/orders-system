95ff1179170fa9fd0f20602f3cf77e5df13e6a3b
guidogamerro <guidogamerro@gmail.com>
2022-06-27 18:16:01 -0300

    Funtion toEntry

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-def entry_str(message,error):
+def entry(message,error):
 
     data = input(message)
 
@@ -9,3 +9,11 @@ def entry_str(message,error):
         data = input(message)
 
     return data
+
+def toEntry():
+
+    print("Welcome to GG Burgers")
+
+    name = entry("Enter your name: ","Error, empty field.")
+
+    return name
